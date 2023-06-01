8dbaf10fe7528e79bdf5da02e44920162cda96b0
k0t9i <k0t9i@yandex.ru>
2023-06-02 02:28:27 +0400

    fix: fix weird renaming

--- src/Shared/Domain/ValueObject/Uuid.php
+++ src/Shared/Domain/ValueObject/Uuid.php
@@ -9,12 +9,12 @@ use TaskManager\Shared\Domain\Exception\InvalidArgumentException;
 
 abstract class Uuid implements Stringable
 {
-    public function __construct(public readonly StringValueObject $value)
+    public function __construct(public readonly string $value)
     {
         $this->ensureIsValidUuid($this->value);
     }
 
-    public function __toString(): StringValueObject
+    public function __toString(): string
     {
         return $this->value;
     }
@@ -24,7 +24,7 @@ abstract class Uuid implements Stringable
         return new static($other->value);
     }
 
-    private function ensureIsValidUuid(StringValueObject $value): void
+    private function ensureIsValidUuid(string $value): void
     {
         $pattern = '/\A[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}\z/Dms';
         if (!preg_match($pattern, $value)) {
