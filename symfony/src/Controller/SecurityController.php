f6d15a3b4160041ff98005129a39d10cf0304b5e
k0t9i <k0t9i@yandex.ru>
2023-06-06 16:38:38 +0400

    feat: add security and user controllers

---
+++ symfony/src/Controller/SecurityController.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SymfonyApp\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+use TaskManager\Shared\Application\Bus\Command\CommandBusInterface;
+use TaskManager\Shared\Application\Service\UuidGeneratorInterface;
+use TaskManager\Users\Application\Command\LoginCommand;
+use TaskManager\Users\Application\Command\RegisterCommand;
+use TaskManager\Users\Infrastructure\Symfony\DTO\UserLoginDTO;
+use TaskManager\Users\Infrastructure\Symfony\DTO\UserRegisterDTO;
+
+#[AsController]
+#[Route('/api/security', name: 'security.')]
+final readonly class SecurityController
+{
+    public function __construct(
+        private CommandBusInterface    $commandBus,
+        private UuidGeneratorInterface $uuidGenerator
+    ) {
+    }
+
+    #[Route('/register/', name: 'register', methods: ['POST'])]
+    public function register(UserRegisterDTO $dto): JsonResponse
+    {
+        $command = new RegisterCommand(
+            $this->uuidGenerator->generate(),
+            $dto->email,
+            $dto->firstname,
+            $dto->lastname,
+            $dto->password,
+            $dto->repeatPassword
+        );
+
+        $this->commandBus->dispatch($command);
+
+        return new JsonResponse(['id' => $command->id], Response::HTTP_CREATED);
+    }
+
+    #[Route('/login/', name: 'login', methods: ['POST'])]
+    public function login(UserLoginDTO $dto): JsonResponse
+    {
+        $command = new LoginCommand(
+            $dto->email,
+            $dto->password
+        );
+
+        $token = $this->commandBus->dispatch($command);
+
+        return new JsonResponse([
+            'token' => $token,
+        ]);
+    }
+}
---
+++ symfony/src/Controller/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SymfonyApp\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+use TaskManager\Shared\Application\Bus\Command\CommandBusInterface;
+use TaskManager\Users\Application\Command\UpdateProfileCommand;
+use TaskManager\Users\Infrastructure\Symfony\DTO\UserProfileUpdateDTO;
+
+#[AsController]
+#[Route('/api/users', name: 'user.')]
+final readonly class UserController
+{
+    public function __construct(
+        private CommandBusInterface $commandBus
+    ) {
+    }
+
+    #[Route('/profile/', name: 'updateProfile', methods: ['PATCH'])]
+    public function updateProfile(UserProfileUpdateDTO $dto): JsonResponse
+    {
+        $command = new UpdateProfileCommand(
+            $dto->firstname,
+            $dto->lastname,
+            $dto->password,
+            $dto->repeatPassword
+        );
+
+        $this->commandBus->dispatch($command);
+
+        return new JsonResponse();
+    }
+}
