32158ceb5a719ffa0bbb514c25e5cfa8c63db40a
k0t9i <k0t9i@yandex.ru>
2023-06-23 20:51:09 +0400

    feat: add base projector

---
+++ src/Projections/Domain/Service/Projector.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TaskManager\Projections\Domain\Service;
+
+use TaskManager\Shared\Domain\Event\DomainEventInterface;
+
+abstract class Projector implements ProjectorInterface
+{
+    /**
+     * @throws \ReflectionException
+     */
+    public function projectWhen(DomainEventInterface $event): void
+    {
+        $this->invokeSuitableMethods($event);
+    }
+
+    /**
+     * @throws \ReflectionException
+     */
+    private function invokeSuitableMethods(DomainEventInterface $event): void
+    {
+        $reflectionObject = new \ReflectionObject($this);
+
+        foreach ($reflectionObject->getMethods() as $method) {
+            if ('projectWhen' === $method->getName()) {
+                continue;
+            }
+            if (1 !== $method->getNumberOfParameters()) {
+                continue;
+            }
+            $typeName = $method->getParameters()[0]->getType()->getName();
+            if (is_a($event, $typeName)) {
+                $method->invoke($this, $event);
+            }
+        }
+    }
+}
