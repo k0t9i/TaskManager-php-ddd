30e8681159c53b12d2b1d0a74bfed4600a9e05af
k0t9i <k0t9i@yandex.ru>
2023-06-24 12:43:26 +0400

    feat: make composite primary key

--- symfony/migrations/Version20230623181715.php
+++ symfony/migrations/Version20230623181715.php
@@ -20,7 +20,7 @@ final class Version20230623181715 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE project_list_projections (id VARCHAR(36) NOT NULL, user_id VARCHAR(36) NOT NULL, name VARCHAR(4000) NOT NULL, finish_date TIMESTAMP(6) WITHOUT TIME ZONE NOT NULL, owner_id VARCHAR(36) NOT NULL, status INT NOT NULL, tasks_count INT NOT NULL, participants_count INT NOT NULL, pending_requests_count INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE project_list_projections (id VARCHAR(36) NOT NULL, user_id VARCHAR(36) NOT NULL, name VARCHAR(4000) NOT NULL, finish_date TIMESTAMP(6) WITHOUT TIME ZONE NOT NULL, owner_id VARCHAR(36) NOT NULL, status INT NOT NULL, tasks_count INT NOT NULL, participants_count INT NOT NULL, pending_requests_count INT NOT NULL, PRIMARY KEY(id, user_id))');
     }
 
     public function down(Schema $schema): void
