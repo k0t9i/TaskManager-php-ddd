fa57389a92193d2afe1567de706153a23981926f
k0t9i <k0t9i@yandex.ru>
2023-06-28 22:37:32 +0400

    feat: check project existence

--- src/Projections/Application/Handler/ProjectRequestQueryHandler.php
+++ src/Projections/Application/Handler/ProjectRequestQueryHandler.php
@@ -7,6 +7,8 @@ namespace TaskManager\Projections\Application\Handler;
 use TaskManager\Projections\Application\Query\ProjectRequestQuery;
 use TaskManager\Projections\Application\Service\CurrentUserExtractorInterface;
 use TaskManager\Projections\Domain\Entity\ProjectRequestProjection;
+use TaskManager\Projections\Domain\Exception\ObjectDoesNotExistException;
+use TaskManager\Projections\Domain\Repository\ProjectProjectionRepositoryInterface;
 use TaskManager\Projections\Domain\Repository\ProjectRequestProjectionRepositoryInterface;
 use TaskManager\Shared\Application\Bus\Query\QueryHandlerInterface;
 
@@ -14,6 +16,7 @@ final readonly class ProjectRequestQueryHandler implements QueryHandlerInterface
 {
     public function __construct(
         private ProjectRequestProjectionRepositoryInterface $repository,
+        private ProjectProjectionRepositoryInterface $projectRepository,
         private CurrentUserExtractorInterface $userExtractor
     ) {
     }
@@ -24,6 +27,11 @@ final readonly class ProjectRequestQueryHandler implements QueryHandlerInterface
     public function __invoke(ProjectRequestQuery $query): array
     {
         $user = $this->userExtractor->extract();
+
+        $project = $this->projectRepository->findById($query->projectId);
+        if (null === $project) {
+            throw new ObjectDoesNotExistException(sprintf('Project "%s" does not exist.', $query->projectId));
+        }
 
         return $this->repository->findAllByProjectIdAndOwnerId($query->projectId, $user->id);
     }
--- src/Projections/Application/Handler/TaskListQueryHandler.php
+++ src/Projections/Application/Handler/TaskListQueryHandler.php
@@ -7,6 +7,8 @@ namespace TaskManager\Projections\Application\Handler;
 use TaskManager\Projections\Application\Query\TaskListQuery;
 use TaskManager\Projections\Application\Service\CurrentUserExtractorInterface;
 use TaskManager\Projections\Domain\Entity\TaskListProjection;
+use TaskManager\Projections\Domain\Exception\ObjectDoesNotExistException;
+use TaskManager\Projections\Domain\Repository\ProjectProjectionRepositoryInterface;
 use TaskManager\Projections\Domain\Repository\TaskListProjectionRepositoryInterface;
 use TaskManager\Shared\Application\Bus\Query\QueryHandlerInterface;
 
@@ -14,6 +16,7 @@ final readonly class TaskListQueryHandler implements QueryHandlerInterface
 {
     public function __construct(
         private TaskListProjectionRepositoryInterface $repository,
+        private ProjectProjectionRepositoryInterface $projectRepository,
         private CurrentUserExtractorInterface $userExtractor
     ) {
     }
@@ -24,6 +27,11 @@ final readonly class TaskListQueryHandler implements QueryHandlerInterface
     public function __invoke(TaskListQuery $query): array
     {
         $user = $this->userExtractor->extract();
+
+        $project = $this->projectRepository->findById($query->projectId);
+        if (null === $project) {
+            throw new ObjectDoesNotExistException(sprintf('Project "%s" does not exist.', $query->projectId));
+        }
 
         return $this->repository->findAllByProjectIdAndUserId($query->projectId, $user->id);
     }
