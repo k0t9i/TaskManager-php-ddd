ca9b6942a270d2958a3ea8f65d95c5a99ce1e503
k0t9i <k0t9i@yandex.ru>
2023-07-03 21:39:02 +0400

    feat: add lockable button component

---
+++ src-frontend/src/components/LockableButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  locked: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <button v-bind="$attrs" :disabled="locked">
+    <slot v-if="locked" name="lockedContent">
+      <span><span class="spinner-border spinner-border-sm" role="status"></span>Loading...</span>
+    </slot>
+    <slot v-else></slot>
+  </button>
+</template>
